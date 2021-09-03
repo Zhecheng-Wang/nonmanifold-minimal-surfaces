8e892ab54b033e08b7fcb33519295e745d89cfdd
Zhecheng-Wang <zhechengwang@utexas.edu>
2021-09-02 22:07:11 -0500

    add comments

--- src/main.cpp
+++ src/main.cpp
@@ -50,6 +50,7 @@ void findBoundaries(const Eigen::MatrixXd& V, const Eigen::MatrixXi& F, Eigen::V
 {
     int nverts = V.rows();
 
+    // floating-point computation tolerance
     double eps = 1e-6;
 
     std::vector<std::vector<int> > bdryL;
@@ -159,6 +160,11 @@ void findBoundaries(const Eigen::MatrixXd& V, const Eigen::MatrixXi& F, Eigen::V
     P.setFromTriplets(Pcoeffs.begin(), Pcoeffs.end());
 }
 
+/**
+ * @brief cross matrix of a 3D vector 
+ * @param v: a 3D vector
+ * @return ret: cross matrix, a 3 x 3 matrix
+ */
 Eigen::Matrix3d crossMatrix(Eigen::Vector3d v)
 {
     Eigen::Matrix3d ret;
@@ -168,6 +174,12 @@ Eigen::Matrix3d crossMatrix(Eigen::Vector3d v)
     return ret;
 }
 
+/**
+ * @brief caculate the derivative and hessian of a triangle area 
+ * @param q0, q1, q2: the three vertices of a triangle, 3D vectors
+ * @return deriv, hess: derivative and hessian of this triangle, pass by reference
+ * @return E: surface area of a triangle
+ */
 double triangleArea(
     Eigen::Vector3d q0,
     Eigen::Vector3d q1,
@@ -178,10 +190,15 @@ double triangleArea(
 {
     Eigen::Vector3d n = (q1 - q0).cross(q2 - q0);
 
+    // E = surface area energy
     double E = 0.5 * n.norm();
 
     if (deriv || hess)
     {
+        /**
+         * Jn = Jacobian of surface area normal
+         * Jn is a 3 x 9 dense matrix
+         */
         Eigen::Matrix<double, 3, 9> Jn;
         Jn.block<3, 3>(0, 0) = crossMatrix(q2 - q1);
         Jn.block<3, 3>(0, 3) = crossMatrix(q0 - q2);
@@ -189,11 +206,19 @@ double triangleArea(
 
         if (deriv)
         {
+            /**
+             * J = Jacobian of surface area energy
+             * J = 1/(2||n||) * n^T * Jn
+             * J is a 1 x 9 dense matrix
+             */
             *deriv = 0.5 / n.norm() * n.transpose() * Jn;
         }
 
         if (hess)
         {
+            /**
+             * B is a 9 x 9 dense matrix
+             */
             Eigen::Matrix<double, 9, 9> B;
             B.setZero();
             B.block<3, 3>(0, 3) = -crossMatrix(n);
@@ -203,9 +228,15 @@ double triangleArea(
             B.block<3, 3>(6, 0) = -crossMatrix(n);
             B.block<3, 3>(6, 3) = crossMatrix(n);
 
+            // I is a 3 x 3 identity matrix
             Eigen::Matrix3d I;
             I.setIdentity();
 
+            /**
+             * H = Hessian of surface area energy
+             * H = 1/2 * (Jn)^T * (I/||n|| - nn^T/(||n||^3)) * Jn + 1/(2||n||) * B
+             * H is a 9 x 9 dense matrix
+             */
             *hess = 0.5 * Jn.transpose() * (I / n.norm() - n * n.transpose() / n.norm() / n.norm() / n.norm()) * Jn + 0.5 / n.norm() * B;
         }
     }
@@ -213,9 +244,17 @@ double triangleArea(
     return E;
 }
 
+/**
+ * @brief map the derivative and hessian of one triangle to the the derivative and hessian of a mesh
+ * @param V: vertices positions of a mesh
+ * @param F: face/connectivity of a mesh
+ * @return deriv, hess: derivative and hessian of a mesh, pass by reference
+ * @return totarea: surface area of a mesh
+ */
 double surfaceArea(const Eigen::MatrixXd& V, const Eigen::MatrixXi& F, Eigen::VectorXd* deriv, std::vector<Eigen::Triplet<double> >* hess)
 {
     int nverts = V.rows();
+
     if (deriv)
     {
         deriv->resize(3 * nverts);
@@ -232,15 +271,18 @@ double surfaceArea(const Eigen::MatrixXd& V, const Eigen::MatrixXi& F, Eigen::Ve
 
     for (int i = 0; i < nfaces; i++)
     {
+        // extract one triangle from the mesh
         Eigen::Vector3i face = F.row(i).transpose();
 
         Eigen::Vector3d q0 = V.row(face[0]).transpose();
         Eigen::Vector3d q1 = V.row(face[1]).transpose();
         Eigen::Vector3d q2 = V.row(face[2]).transpose();
 
+        // get derivative and hessian of this triangle
         double a = triangleArea(q0, q1, q2, deriv ? &aderiv : NULL, hess ? &ahess : NULL);
         totarea += a;
 
+        // map derivative
         if (deriv)
         {
             for (int j = 0; j < 3; j++)
@@ -252,6 +294,7 @@ double surfaceArea(const Eigen::MatrixXd& V, const Eigen::MatrixXi& F, Eigen::Ve
             }
         }
 
+        // map hessian
         if (hess)
         {
             for (int j = 0; j < 3; j++)
@@ -269,9 +312,16 @@ double surfaceArea(const Eigen::MatrixXd& V, const Eigen::MatrixXi& F, Eigen::Ve
             }
         }
     }
+
     return totarea;
 }
 
+/**
+ * @brief caculate the derivative and willmore energy of a triangle 
+ * @param q0, q1, q2: the three vertices of a triangle, 3D vectors
+ * @return deriv: derivative of this triangle's willmore energy, pass by reference
+ * @return E: willmore energy of a triangle
+ */
 double triangleWillmore(
     Eigen::Vector3d q0,
     Eigen::Vector3d q1,
@@ -288,21 +338,32 @@ double triangleWillmore(
 
     if (deriv)
     {
+        // Jwillmore = 2 * J * H
         *deriv = 2.0 * areaderiv * areahess;
     }
 
     return E;
 }
 
+/**
+ * @brief map the derivative and hessian of one triangle to the the derivative and hessian of a mesh
+ * @param V: vertices positions of a mesh
+ * @param F: face/connectivity of a mesh
+ * @return deriv: derivative of a mesh's willmore energy, pass by reference
+ * @return tot: total willmore energy of a mesh
+ */
 double willmore(const Eigen::MatrixXd& V, const Eigen::MatrixXi& F, Eigen::VectorXd* deriv)
 {
     int nverts = V.rows();
+
+    // if deriv not nullptr, reset deriv vector
     if (deriv)
     {
         deriv->resize(3 * nverts);
         deriv->setZero();
     }
     
+    // init derivative and hessian of one triangle
     Eigen::Matrix<double, 1, 9> aderiv;
     Eigen::Matrix<double, 9, 9> ahess;
 
@@ -311,15 +372,18 @@ double willmore(const Eigen::MatrixXd& V, const Eigen::MatrixXi& F, Eigen::Vecto
 
     for (int i = 0; i < nfaces; i++)
     {
+        // extract one triangle from the mesh
         Eigen::Vector3i face = F.row(i).transpose();
 
         Eigen::Vector3d q0 = V.row(face[0]).transpose();
         Eigen::Vector3d q1 = V.row(face[1]).transpose();
         Eigen::Vector3d q2 = V.row(face[2]).transpose();
 
+        // get derivative and willmore energy of this triangle
         double E = triangleWillmore(q0, q1, q2, deriv ? &aderiv : NULL);
         tot += E;
 
+        // map derivative
         if (deriv)
         {
             for (int j = 0; j < 3; j++)
@@ -331,10 +395,19 @@ double willmore(const Eigen::MatrixXd& V, const Eigen::MatrixXi& F, Eigen::Vecto
             }
         }
     }
+
     return tot;
 }
 
-
+/**
+ * @brief newton's method solver
+ * @param independent: indepedent set of vertices positions on a mesh
+ * @param F: face/connectivity of a mesh
+ * @param P: selection matrix of indepedent set
+ * @param b: boundary vertices of a mesh
+ * @return newindependent: updated indepedent set of vertices positions, pass by reference
+ * @return gradient, descentDir: graident and descent direction (per vertex) of a mesh, pass by reference
+ */
 void newton(const Eigen::VectorXd &independent, const Eigen::MatrixXi& F, const Eigen::SparseMatrix<double> &P, const Eigen::VectorXd &b, Eigen::VectorXd &newindependent, Eigen::MatrixXd &gradient, Eigen::MatrixXd &descentDir)
 {
     Eigen::VectorXd flatV = P * independent + b;
@@ -347,22 +420,23 @@ void newton(const Eigen::VectorXd &independent, const Eigen::MatrixXi& F, const 
 
     Eigen::VectorXd deriv;
     std::vector<Eigen::Triplet<double> > hesscoeffs;
-    double oldenergy = willmore(V, F, &deriv);
-
+    double oldenergy = willmore(V, F, &deriv);  // note that we do not need hessian for willmore energy
     
-    Eigen::SparseMatrix<double> L(nverts, nverts);    
-    Eigen::SparseMatrix<double> M(nverts, nverts);    
+    Eigen::SparseMatrix<double> L(nverts, nverts);
+    Eigen::SparseMatrix<double> M(nverts, nverts);
+    // L = stiffness matrix/discrete laplacian of a mesh
     igl::cotmatrix(V, F, L);
+    // M = mass matrix of a mesh, computed with barycentric coords
     igl::massmatrix(V, F, igl::MASSMATRIX_TYPE_BARYCENTRIC, M);
 
-    std::vector<Eigen::Triplet<double> > bigLcoeffs;    
+    std::vector<Eigen::Triplet<double> > bigLcoeffs;
     for (int k=0; k<L.outerSize(); ++k)
         for (Eigen::SparseMatrix<double>::InnerIterator it(L,k); it; ++it)
         {
             for (int j = 0; j < 3; j++)
             {
                 bigLcoeffs.push_back({ 3 * (int)it.row() + j, 3 * (int)it.col() + j, it.value() });
-            }            
+            }
         }
 
     std::vector<Eigen::Triplet<double> > bigMcoeffs;
@@ -382,6 +456,7 @@ void newton(const Eigen::VectorXd &independent, const Eigen::MatrixXi& F, const 
     Eigen::SparseMatrix<double> projM = P.transpose() * bigM * P;
 
     Eigen::SparseMatrix<double> projL = P.transpose() * bigL.transpose() * P;
+    // init solver to solve projL*y = -projJ for y and projL*dx = projM*y for dx
     Eigen::SPQR<Eigen::SparseMatrix<double> > Lsolver(projL);
     Eigen::VectorXd projJ = P.transpose() * deriv;
     Eigen::VectorXd rhs = -projJ;
@@ -389,12 +464,14 @@ void newton(const Eigen::VectorXd &independent, const Eigen::MatrixXi& F, const 
     Eigen::VectorXd rhs2 = projM * y;
     Eigen::VectorXd dx = Lsolver.solve(rhs2);
 
+    // compute the residuals and print out
     double resid1 = (projL * dx - rhs2).norm();
     double resid2 = (projL * y - rhs).norm();
 
     std::cout << std::endl;
     std::cout << "Solver residuals: " << resid1 << " " << resid2 << std::endl;
 
+    // backtracking line search
     Eigen::VectorXd deltaV = P * dx;
 
     double step = 1.0;
@@ -403,7 +480,9 @@ void newton(const Eigen::VectorXd &independent, const Eigen::MatrixXi& F, const 
         Eigen::MatrixXd newV = V;
         for (int i = 0; i < nverts; i++)
             newV.row(i) += step * deltaV.segment<3>(3 * i);
+        // compute the updated energy with the new vertices
         double newenergy = willmore(newV, F, NULL);
+        // if the updated energy is lower, break the loop
         if (newenergy <= oldenergy)
         {
             std::cout << "Accepted step " << step << std::endl;
@@ -419,10 +498,14 @@ void newton(const Eigen::VectorXd &independent, const Eigen::MatrixXi& F, const 
             }
             return;
         }
+        // if the current step is too aggressive, halve the step and retry
         step /= 2;
     }
 }
 
+/**
+ * @brief main function
+ */
 int main(int argc, char *argv[])
 {
     Eigen::MatrixXd V;
@@ -430,6 +513,8 @@ int main(int argc, char *argv[])
 
     std::string objfile = "cube.obj";
 
+    // find obj file in current folder and its parent folder
+    // load vertices (V) and faces (F)
     Eigen::MatrixXd values;
     if (!igl::readOBJ(objfile, V, F))
     {
@@ -451,6 +536,7 @@ int main(int argc, char *argv[])
     double noisemag = 1e-4;
     independent = independent + noisemag * noise;
     
+    // test mesh set-up
     /*V.resize(9,3);
     V << -1, -1, 0,
         0, -1, 0,
@@ -478,13 +564,16 @@ int main(int argc, char *argv[])
     Eigen::VectorXd newindependent;
     Eigen::MatrixXd gradient;
     Eigen::MatrixXd descentDir;
+    // add reference/initial mesh to viewer
     auto *prevMesh = polyscope::registerSurfaceMesh("Mesh 0", V, F);
+    
+    // solver iteration steps setting
+    int noutersteps = 1;    // number of minimal surface optimization steps
+    int ninnersteps = 100;  // number of newton's method steps
 
-    int num_step = 100;
-
-    for (int outer = 0; outer < 1; outer++)
+    for (int outer = 0; outer < noutersteps; outer++)
     {
-        for (int inner = 0; inner < num_step; inner++)
+        for (int inner = 0; inner < ninnersteps; inner++)
         {
             newton(independent, F, P, b, newindependent, gradient, descentDir);
             independent = newindependent;
@@ -494,11 +583,12 @@ int main(int argc, char *argv[])
         for (int i = 0; i < nverts; i++)
             newV.row(i) = newVflat.segment<3>(3 * i);
 
+        // add the mesh at this optimization step tp viewer
         prevMesh = polyscope::registerSurfaceMesh("Mesh " + std::to_string(outer+1), newV, F);
         prevMesh->addVertexVectorQuantity("Gradient", gradient);
         prevMesh->addVertexVectorQuantity("Descent Dir", descentDir);        
     }
     
-    // visualize!
+    // visualize
     polyscope::show();
 }
